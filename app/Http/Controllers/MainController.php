316b7f5f982fca4ec4be8812f312c5e54151a7dd
Diego Torino <diegotorinodev@gmail.com>
2024-12-10 19:31:53 -0300

    003

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,16 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public function home()
+    public function home():View
     {
-        echo 'apresentar a pagina inicial';
+        return view('home');
     }
     public function generateExercises(Request $request)
     {
-        echo 'Gerar exercicios';
+        // form validation
+        $request->validate([
+            'check_sum' => 'required_without_all:check_subtraction,check_multiplication,check_division',
+            'check_subtraction' => 'required_without_all:check_sum,check_multiplication,check_division',
+            'check_multiplication' => 'required_without_all:check_sum,check_subtraction,check_division',
+            'check_division' => 'required_without_all:check_sum,check_subtraction,check_multiplication',
+            'number_one' => 'required|integer|min:0|max:999',
+            'number_two' => 'required|integer|min:0|max:999',
+            'number_exercises' => 'required|integer|min:5|max:50',
+        ]);
+
+        dd($request->all());
     }
 
     public function printExercises()
